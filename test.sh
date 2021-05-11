f6a3669ce3890f4031bda365c86cbaafd74ec4eb
Koray <koray.ak@gdi-service.de>
2021-05-11 17:34:14 +0300

    Added -n1 for read command

--- test.sh
+++ test.sh
@@ -1,27 +1,24 @@
 echo -e "Giris yapmak icin 1'e, Cikis yapmak icin 2'ye basiniz: \c"
-read result
-
-#case $result in
-#*\ * )
-#echo "gecersiz deger";;
-#esac
+read -n1 result
 
 
-if ! [[ "$result" =~ ^[1-2]$ ]] 
+	if ! [[ "$result" =~ ^[1-2]$ ]] 
 then
-echo "1 veya 2 girmeniz gerekmektedir"
+	echo " "
+	echo "1 veya 2 girmeniz gerekmektedir"
 else
 
-
-if [ "$result" == 1 ]
+	if [ "$result" == 1 ]
 then
-echo "updated"
+	echo " "
+	echo "updated"
 fi
 
-if [ "$result" == 2 ]
+	if [ "$result" == 2 ]
 then
-echo "Cikis yaptiniz"
+	echo " "
+	echo "Cikis yaptiniz"
 exit 1
-fi
 
+fi
 fi
